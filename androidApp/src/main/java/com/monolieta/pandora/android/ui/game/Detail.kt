6141b6c0f4645211347170426ee44433dd9ab561
Jonattan <jonattanva89@gmail.com>
2021-09-03 11:36:22 -0500

    Se incluye módulo detalle

--- androidApp/src/main/java/com/monolieta/pandora/android/ui/game/Detail.kt
+++ androidApp/src/main/java/com/monolieta/pandora/android/ui/game/Detail.kt
@@ -1,6 +1,7 @@
 package com.monolieta.pandora.android.ui.game
 
 import android.content.res.Configuration
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -20,15 +21,33 @@ import com.monolieta.pandora.android.ui.component.Tab
 import com.monolieta.pandora.database.Game
 
 @Composable
-fun DetailGame(game: Game) {
-    Column(modifier = Modifier.background(Color.Black)) {
-
+fun Detail(@DrawableRes banner: Int, @DrawableRes cover: Int, game: Game) {
+    Detail(game = game) {
         Banner(
-            banner = R.drawable.psychonauts_banner,
-            cover = R.drawable.psychonauts,
+            banner = banner,
+            cover = cover,
             title = game.name,
             subtitle = game.developer
         )
+    }
+}
+
+@Composable
+fun Detail(game: Game) {
+    Detail(game = game) {
+        Banner(
+            banner = game.screenshots.random(),
+            cover = game.cover,
+            title = game.name,
+            subtitle = game.developer
+        )
+    }
+}
+
+@Composable
+private fun Detail(game: Game, content: @Composable () -> Unit) {
+    Column(modifier = Modifier.background(Color.Black)) {
+        content()
 
         Spacer(modifier = Modifier.height(4.dp))
 
@@ -90,6 +109,7 @@ fun DetailGame(game: Game) {
     }
 }
 
+
 @Preview(name = "Light Mode")
 @Preview(
     name = "Dark Mode",
@@ -98,8 +118,10 @@ fun DetailGame(game: Game) {
 )
 @Composable
 private fun Preview() {
-    DetailGame(
-        Game(
+    Detail(
+        banner = R.drawable.psychonauts_banner,
+        cover = R.drawable.psychonauts,
+        game = Game(
             key = "ea33b427-7483-4be4-a2f9-ac9cc4426cb1",
             release = 1632459600000,
             cover = "ea33b427-7483-4be4-a2f9-ac9cc4426cb1.webp",
